37c032fe9c715325fadb1a09305a1b77d5563bb6
Donaven Snowden <donaven.snowden@gmail.com>
2021-07-22 02:21:26 -0700

    refactor JS, accomodate rails page loads with turbolinks:load

--- app/javascript/custom.js
+++ app/javascript/custom.js
@@ -1,5 +1,5 @@
-
-      window.addEventListener('load', setup);
+      // change this from load to turbolinks:load so that this event is fired every time there is a page change in rails.
+      window.addEventListener('turbolinks:load', setup);
       // use global variables for these so they can be accessed inside of function scopes
       var myChart;
       var vue;
@@ -12,61 +12,57 @@
         vue = new Vue({
             el: '#app',
             data: {
-                builtData: null,
-                selected: null,
+                // builtData: buildData(breakdownData, categories[0]),
+                selected: categories[0],
                 options: categories
+              },
+            computed: {
+                builtData: function () {
+                    // Make sure that Objects are not re-assembled into key array and value array out of order.
+                    var keys = Object.keys(breakdownData[this.selected]);
+                    keys.sort();
+                    const keyArray = [];
+                    const valueArray = [];
+                    for(var i=0; i<keys.length; ++i){
+                        keyArray.push(keys[i]);
+                        valueArray.push(breakdownData[this.selected][keys[i]]);
+                    }
+                    // console.log({ keyArray, valueArray })
+                    return { keyArray, valueArray }
+                }
               },
             methods: {
                 recreateChart() {
-                    this.builtData = buildData(breakdownData, this.selected);
-                    // recreateChart2()
                     myChart.destroy();
                     buildChart()
                 }
             }
         })
 
-        vue.$data.builtData = buildData(breakdownData, categories[0]);
-
         buildChart();
 
     }
 
     function buildChart() {
-    const ctx = document.getElementById('myChart').getContext('2d');
-    myChart = new Chart(ctx, {
-        type: 'pie',
-        data: {
-            labels: vue.$data.builtData.keyArray,
-            datasets: [
-            {
-                label: vue.$data.selected,
-                data: vue.$data.builtData.valueArray,
-                fill: false,
-                // https://coolors.co/fec5bb-fcd5ce-fae1dd-f8edeb-e8e8e4-d8e2dc-ece4db-ffe5d9-ffd7ba-fec89a
-                backgroundColor : ["#FEC5BB", "#FCD5CE", "#FAE1DD", "#F8EDEB", "#E8E8E4","#D8E2DC","#ECE4DB","#FFE5D9","#FFD7BA","#FEC89A"],
-                borderWidth: 1
-            }
-            ]
-        },
-        options: { responsive: false, maintainAspectRatio: true }
+        const ctx = document.getElementById('myChart').getContext('2d');
+        myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: vue.builtData.keyArray,
+                datasets: [
+                {
+                    label: vue.$data.selected,
+                    data: vue.builtData.valueArray,
+                    fill: false,
+                    // https://coolors.co/fec5bb-fcd5ce-fae1dd-f8edeb-e8e8e4-d8e2dc-ece4db-ffe5d9-ffd7ba-fec89a
+                    backgroundColor : ["#FEC5BB", "#FCD5CE", "#FAE1DD", "#F8EDEB", "#E8E8E4","#D8E2DC","#ECE4DB","#FFE5D9","#FFD7BA","#FEC89A"],
+                    borderWidth: 1
+                }
+                ]
+            },
+            options: { responsive: false, maintainAspectRatio: true }
         });
     }
-
-    function buildData(object, category) {
-        // Make sure that Objects are not re-assembled into key array and value array out of order.
-        var keys = Object.keys(object[category]);
-        keys.sort();
-        const keyArray = [];
-        const valueArray = [];
-        for(var i=0; i<keys.length; ++i){
-            keyArray.push(keys[i]);
-            valueArray.push(object[category][keys[i]]);
-        }
-        return { keyArray, valueArray }
-
-    }
-
 
     async function getData() {
         return fetch('/data')
