ebd45a92e626f8f8d904e3dc4d1fd1b145a7b93c
Donaven Snowden <donaven.snowden@gmail.com>
2021-07-22 00:47:23 -0700

    swap out sqlite3 for pg for heroku deploy

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_21_083921) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "data_sets", force: :cascade do |t|
     t.text "FIRST NAME"
     t.text "MIDDLE INITIAL"
